77699641e656d1d90fbd46945496b80ad8041fc2
Иван Радостинов Драганов <IRDraganov20@codingburgas.bg>
2023-10-10 09:35:19 +0300

    Initial commit

---
+++ src/bro.java
@@ -0,0 +1,10 @@
+public class bro
+{
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        System.out.println("1 2 3 4 5");
+        System.out.println("1 2 3 4 5");
+        System.out.println("1 2 3 4 5");
+    }
+}
